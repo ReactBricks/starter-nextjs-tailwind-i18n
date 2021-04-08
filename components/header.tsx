1ee31cf833d79e30fe44fd6a7033498889c70d12
Matteo Frana <f2@f2.net>
2021-04-08 10:28:26 +0200

    Next i18n routing

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 
 const Header: React.FC = () => (
-  <header className="bg-white h-20 py-5 border-b sticky top-0">
+  <header className="bg-white sm:h-20 py-5 border-b sm:sticky top-0">
     <div className="max-w-5xl mx-auto px-6">
       <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center">
         <div className="flex flex-col sm:flex-row items-center mb-4 sm:mb-0">
@@ -11,20 +11,33 @@ const Header: React.FC = () => (
             className="w-48"
             alt="React Bricks"
           />
-          <div className="ml-8 flex space-x-5 text-center">
+          <div className="sm:ml-8 flex space-x-5 text-center">
             <Link href="/">
               <a className="text-gray-500 hover:text-pink-700">Home</a>
             </Link>
-            <Link href="/about-us">
+            <Link href="about-us">
               <a className="text-gray-500 hover:text-pink-700">About us</a>
             </Link>
           </div>
         </div>
-        <Link href="/admin">
-          <a className="py-2 px-5 rounded text-white font-medium bg-azure-500 hover:bg-azure-600 hover:shadow-lg transition duration-200">
-            Edit content
-          </a>
-        </Link>
+        <div className="flex items-center space-x-6">
+          <div className="flex items-center space-x-2">
+            <Link href="/" locale="en">
+              <a className="text-gray-500 hover:text-pink-700">EN</a>
+            </Link>
+            <Link href="/" locale="it">
+              <a className="text-gray-500 hover:text-pink-700">IT</a>
+            </Link>
+            <Link href="/" locale="es">
+              <a className="text-gray-500 hover:text-pink-700 pr-6">ES</a>
+            </Link>
+          </div>
+          <Link href="/admin">
+            <a className="py-2 px-5 rounded text-white font-medium bg-cyan-500 hover:bg-cyan-600 hover:shadow-lg transition duration-200">
+              Edit content
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   </header>
